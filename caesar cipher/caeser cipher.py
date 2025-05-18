3b6bb6d1a54b7b0ff30c8ef4e2f432d167b2ccba
Mfonobong Emmanuel <141635798+Mfonjames15@users.noreply.github.com>
2025-05-19 00:24:11 +0100

    Update caeser cipher.py
    This project implements the classic Caesar Cipher algorithm in Python. It allows users to encrypt and decrypt text by shifting letters by a specified number of positions in the alphabet.

--- caesar cipher/caeser cipher.py
+++ caesar cipher/caeser cipher.py
@@ -2,19 +2,21 @@ import string
 
 chars = " " + string.ascii_letters + string.digits + string.punctuation
 chars = list(chars)
-print(chars)
+
 shift = int(input("Enter the shift value: "))
 
 keys = chars[shift:] + chars[:shift]
-print(keys)
 
 # ENCRYPTED TEXT
 plain_text = input("\nEnter the text to be encrypted: ")
 cipher_text = ""
 
 for letter in plain_text:
-    index = chars.index(letter)
-    cipher_text += keys[index]
+    if letter in chars:
+        index = chars.index(letter)
+        cipher_text += keys[index]
+    else:
+        cipher_text += letter
 
 print(f"\nPLAIN TEXT IS: {plain_text}")
 print(f"CIPHER TEXT IS: {cipher_text}")
@@ -24,8 +26,11 @@ cipher_text = input("\nEnter the text to be decrypted: ")
 plain_text = ""
 
 for letter in cipher_text:
-    index = keys.index(letter)
-    plain_text += chars[index]
+    if letter in keys:
+        index = keys.index(letter)
+        plain_text += chars[index]
+    else:
+        plain_text += letter
 
 print(f"\nCIPHER TEXT IS: {cipher_text}")
 print(f"PLAIN TEXT IS: {plain_text}")
